c81b2b0971a8955143588f6646f1e33f323aa728
OsamaImadi <osamaimadi@yahoo.com>
2020-08-26 14:47:49 +0500

    Changed page url

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -77,7 +77,7 @@ import {
      <div style={{display: 'flex', justifyContent: 'center'}}>
         <LargeButton
           inverted
-          href="/birthday"
+          href="/wish"
           mb={4}
         >
           Continue
--- src/components/secondBio.js
+++ src/components/secondBio.js
@@ -11,6 +11,11 @@ const SecondBio = () => (
     Wishing you a day filled with happiness and a year filled with joy!
     </Heading.h2>
     <Heading.h4  color="white" align="center" f={[3, 4]} mt={3} regular>
+    Baaqi yahan tu ne jo wish krna hai woh likh dun ga.
+    Plus pictures ko jo bhi caption deni hain woh bta den
+    warna isi caption k saath deploy hogi,
+    Adjacent pics ki caption word count same honi chahye.
+    <br/>
       Here are some of your pictures please take a look.
     </Heading.h4>
   </Container>
